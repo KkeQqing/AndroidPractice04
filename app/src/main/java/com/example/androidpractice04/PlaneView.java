8adbcb5ab7b3584902d43d5a50724fce33f12874
kq <1798178975@qq.com>
2026-03-17 17:19:24 +0800

    .

---
+++ app/src/main/java/com/example/androidpractice04/PlaneView.java
@@ -0,0 +1,30 @@
+package com.example.androidpractice04;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+public class PlaneView {
+    public float currentX;
+    public float currentY;
+    Bitmap plane;
+
+    public PlaneView(Context context){
+        super(context);
+        //定义飞机图片
+        plane = BitmapFactory.decodeResource(context.getResources(),
+                R.drawable.plane);
+        setFocusable(true);
+    }
+
+    @Override
+    public void onDraw(Canvas canvas){
+        super.onDraw(canvas);
+        //创建画笔
+        Paint p = new Paint();
+        // 绘制飞机
+        canvas.drawBitmap(plane,currentX,currentY,p);
+    }
+}
